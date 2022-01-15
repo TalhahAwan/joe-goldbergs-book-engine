816168bc3ea2e2b4b10a7896edd62a235c09acec
Talhah Awan <talhahawan@gmail.com>
2022-01-15 16:37:06 -0600

    better template

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,8 +1,8 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_PROFILES = gql`
-  query allProfiles {
-    profiles {
+export const QUERY_ME = gql`
+  query me {
+    me {
       _id
       name
       skills
