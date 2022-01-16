5d1abfb9690f25a7b92d3da9df0b0f6705de62be
Talhah Awan <talhahawan@gmail.com>
2022-01-15 19:41:00 -0600

    added type book data

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,10 +2,20 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
     type User {
+        _id: ID!,
+        username: String!,
+        savedBooks: [Book],
+        bookCount: Int
 
     }
 
     type Book {
+        bookId: String!,
+        title: String!
+        author: [String],
+        description: String!,
+        image: String,
+        link: String,
 
     }
 
