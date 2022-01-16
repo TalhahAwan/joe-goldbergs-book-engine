9531b19ab8ec8aef717d0b309f6820cf307d91aa
Talhah Awan <talhahawan@gmail.com>
2022-01-15 18:36:29 -0600

    added new info in try catch method

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -28,14 +28,9 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
+      const { data } = await addUser({
+        variables: { ...userFormData},
+      })
       Auth.login(token);
     } catch (err) {
       console.error(err);
