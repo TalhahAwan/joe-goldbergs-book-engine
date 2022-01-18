c1545d224674b21f50a427da9fcd71b54ad3f483
Talhah Awan <talhahawan@gmail.com>
2022-01-17 19:21:54 -0600

    added userData into query resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,17 +4,11 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    profiles: async () => {
-      return Profile.find();
-    },
 
-    profile: async (parent, { profileId }) => {
-      return Profile.findOne({ _id: profileId });
-    },
-    // By adding context to our query, we can retrieve the logged in user without specifically searching for them
     me: async (parent, args, context) => {
       if (context.user) {
-        return Profile.findOne({ _id: context.user._id });
+          const userData = await User.findOne({ _id: context.user._id}).select('-__v -password')
+        return userData;
       }
       throw new AuthenticationError('You need to be logged in!');
     },
