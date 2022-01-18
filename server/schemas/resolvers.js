8fb5517370aff7d1f3ecd6c59867b958143f8789
Talhah Awan <talhahawan@gmail.com>
2022-01-17 19:15:23 -0600

    required user and book

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { Profile } = require('../models');
+const { User, Book } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
